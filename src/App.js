f51b4551507a643282066d4b13720be27bf50ea2
Ayush <you@example.com>
2021-11-19 08:51:09 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Switch>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+
+
